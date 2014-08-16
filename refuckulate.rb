0dc9a53730c05e9c7f71158df74326775aeb05d9
Robert D. French <robert@robertdfrench.me>
2014-08-16 15:49:14 -0400

    Check for CMakeLists.txt

---
+++ refuckulate.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+# CMake Refuckulator
+# Converts CMake projects into Autotools projects
+# Copyright 2014 Robert D. French
+
+class Logger
+	def initialize
+		@log_count = 0
+	end
+
+	def log(msg)
+		@log_count += 1
+		puts "[CMKREF #{@log_count}] #{msg}"
+	end
+end
+
+def log(msg)
+	@logger.log(msg)
+end
+
+def is_cmake?
+	File.exists? "CMakeLists.txt"
+end
+
+def banner
+	log "Checking to see if your project is totally fucked"
+end
+
+def main
+	@logger = Logger.new
+	banner
+
+	log "Looking around for CMakeLists.txt or any other greasy bullshit"
+	if is_cmake?
+		log "Your project is fucked in the head."
+	else
+		log "Okay, move along. Nothing here to see."
+	end
+end
+
+main
